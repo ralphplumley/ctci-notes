d5d53f13d69cb95da76d196bf0de76a49c7e3859
Ralph Plumley <ralphplumley@gmail.com>
2018-05-04 10:54:43 -0400

    complete ch1_4.js

--- ch1_4.js
+++ ch1_4.js
@@ -1,1 +1,32 @@
-// Palindrom Permutation
+// Palindrome Permutation
+
+// I:  Tact Coa
+// O: true 
+
+function isPalindrome(input) {
+  let tally = {};
+  let hasPivotCharacterAlready = false;
+
+  for(var i = 0; i < input.length; i++ ) {
+
+    if (input[i] === ' ') {
+      continue;
+    }
+
+    let currentCharacter = input[i];
+    if (tally.hasOwnProperty(currentCharacter)) {
+      if(hasPivotCharacterAlready) {
+        return false;
+      } else {
+        hasPivotCharacterAlready = true;
+        tally[currentCharacter]++;
+      }
+    } else {
+      tally[currentCharacter] = 1;
+    }
+  }
+
+  return true;
+}
+
+console.log(isPalindrome('Tact Coa')); // true
